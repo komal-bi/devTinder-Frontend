5a23849f34131d42f80e9531c2e14ebc45e17a7e
Komal More <komalmore31724@gmail.com>
2026-05-28 15:50:51 +0530

    made url synamic

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,13 +1,13 @@
+let BaseUrl =
+  location.hostname == "localhost" ? "http://localhost:7777" : "/api";
 
-let BaseUrl='/api'
-
-export const LOGIN_URL= `${BaseUrl}/login`;
-export const SIGNUP_URL= `${BaseUrl}/signup`;
-export const GET_PROFILE_URL= `${BaseUrl}/profile/view`;
-export const GET_FEED_LIST= `${BaseUrl}/user/feed`;
-export const REQUEST_SEND= `${BaseUrl}/request/send`;
-export const REQUEST_REVIEW= `${BaseUrl}/request/review`;
-export const LOGOUT= `${BaseUrl}/logout`;
-export const CONNECTIONS= `${BaseUrl}/user/connections`;
-export const REQUESTS_RECEIVED= `${BaseUrl}/user/requests/received`;
-export const UPDATE_PROFILE=`${BaseUrl}/profile/edit`
+export const LOGIN_URL = `${BaseUrl}/login`;
+export const SIGNUP_URL = `${BaseUrl}/signup`;
+export const GET_PROFILE_URL = `${BaseUrl}/profile/view`;
+export const GET_FEED_LIST = `${BaseUrl}/user/feed`;
+export const REQUEST_SEND = `${BaseUrl}/request/send`;
+export const REQUEST_REVIEW = `${BaseUrl}/request/review`;
+export const LOGOUT = `${BaseUrl}/logout`;
+export const CONNECTIONS = `${BaseUrl}/user/connections`;
+export const REQUESTS_RECEIVED = `${BaseUrl}/user/requests/received`;
+export const UPDATE_PROFILE = `${BaseUrl}/profile/edit`;
